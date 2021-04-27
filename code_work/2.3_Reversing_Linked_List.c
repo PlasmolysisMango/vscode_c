c8e1edc2c71824aa04a34b8ae36439f8365712a5
PlasmolysisMango <mrgrandpa@163.com>
2021-04-27 18:26:29 +0800

    20210427_2

--- code_work/2.3_Reversing_Linked_List.c
+++ code_work/2.3_Reversing_Linked_List.c
@@ -1,14 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct ChainList *List;
+//思路：利用堆栈，避免对链表的重复修改。
+
+typedef struct ChainNode *Stack;
+typedef struct ChainNode *List;
 typedef int Address;
-struct ChainList
+struct ChainNode
 {
     Address Addr;
     int Data;
-    Address Next;
+    Address NextAddr;
+    struct ChainNode *Link;
 };
 
-//思路：利用堆栈加二分查找的方法，避免对链表的重复修改。
+Stack CreateStack()
+{
+    Stack S;
+    S = (Stack)malloc(sizeof(struct ChainNode));
+    S->Link = NULL;
+    S->NextAddr = -1;
+    return S;
+}
+
+void SearchPush(Address A, List L, Stack S)
+{
+    while (L->Link)
+    {
+        if (A == L->Link->Addr)
+        {
+            List tmpnode;
+            tmpnode = L->Link;
+            L->Link = tmpnode->Link;
+            L->NextAddr = tmpnode->NextAddr;
+            tmpnode->Link = S->Link;
+            tmpnode->NextAddr = S->NextAddr;
+            S->Link = tmpnode;
+            S->NextAddr = tmpnode->Addr;
+            return;
+        }
+        L = L->Link;
+    }
+}
+
+void PopPrint(Stack S)
+{
+    Stack p;
+    int d;
+    p = S->Link;
+    S->Link = p->Link;
+    d = p->Data;
+    if (!p->Link)
+    {
+        printf("%05d %d %d\n", p->Addr, p->Data, p->NextAddr);
+    }
+    else
+    {
+        printf("%05d %d %05d\n", p->Addr, p->Data, p->NextAddr);
+    }     
+    free(p);
+}
+
+List ReadList(int N)
+{
+    List L, Rear, newnode;
+    Rear = L = (List)malloc(sizeof(struct ChainNode));
+    while (N--)
+    {
+        newnode = (List)malloc(sizeof(struct ChainNode));
+        scanf("%d %d %d", &newnode->Addr, &newnode->Data, &newnode->NextAddr);
+        newnode->Link = NULL;
+        newnode->NextAddr = -1;
+        Rear->Link = newnode;
+        Rear->NextAddr = newnode->Addr;
+        Rear = newnode;
+    }
+    return L;
+}
+
+int main()
+{
+    int N, K;
+    Address FAddr;
+    List L;
+    Stack S;
+    scanf("%d %d %d", &FAddr, &N, &K);
+    L = ReadList(N);
+    S = CreateStack();
+    while (FAddr != -1)
+    {
+        for (int i = 0; i < K; i++)
+        {
+            if (!L->Link)
+            {
+                break;
+            }
+            SearchPush(FAddr, L, S);
+            FAddr = S->Link->NextAddr;
+        }
+        while (S->Link)
+        {
+            PopPrint(S);
+        }
+    }
+}
+
 
