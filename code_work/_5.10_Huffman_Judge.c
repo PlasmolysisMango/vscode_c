7300d4f2aa7572667783c3bcca7f67e590d14adb
PlasmolysisMango <mrgrandpa@163.com>
2021-06-03 19:59:27 +0800

    20210603

--- code_work/_5.10_Huffman_Judge.c
+++ code_work/_5.10_Huffman_Judge.c
@@ -3,12 +3,33 @@
 #include <string.h>
 
 #define MaxLenth 63
+#define MaxM 1000
 
-typedef struct TreeNode
+typedef struct TreeNode //用于存储Huffman树
 {
     struct TreeNode *Left;
     struct TreeNode *Right;
 } *HuffmanTree;
+
+typedef char Code[MaxLenth + 1]; //用于读入Huffman编码
+
+typedef int Frequence; //用于存储字符出现的频率
+
+void BubbleSort(Frequence *F, int N) //用于排序频率序列
+{
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            if (F[j] < F[j + 1])
+            {
+                int temp = F[j];
+                F[j] = F[j + 1];
+                F[j + 1] = temp;
+            }
+        }
+    }
+}
 
 HuffmanTree NewNode()
 {
